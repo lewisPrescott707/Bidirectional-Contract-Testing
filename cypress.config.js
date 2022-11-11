0aaef0ef8c49ed09c5019656cc373f36627bf770
prescottlewis707 <prescottlewis707@gmail.com>
2022-11-11 13:20:26 +0000

    Revert "starting point"
    This reverts commit 84f39f088a930d936fa67b2bf136fb36ef27d12d.

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require('cypress')
 // require pact cypress plugin & fs
+const pactCypressPlugin = require('@pactflow/pact-cypress-adapter/dist/plugin')
+const fs = require('fs')
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // pact cypress plugin
+      pactCypressPlugin(on, config, fs)
       return (on, config)
     },
     baseUrl: 'http://localhost:3000',
